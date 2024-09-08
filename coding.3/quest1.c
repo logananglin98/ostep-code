8ae17d5146cdccbebf7ceda51215fad2909b5185
logananglin98 <logananglin98@gmail.com>
2024-09-08 19:33:04 -0400

    Commiting files so I can clone them in WSL

---
+++ coding.3/quest1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int adgc, char *argv[]) {
+    int x = 100;
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+        printf("Child process value: %d", x);
+    } else {
+        printf("Parent process value: %d", x);
+    }
+    return 0;
+}
