1f4a5ad7670c2f50a10fe7f579d4600ed98184fd
logananglin98 <logananglin98@gmail.com>
2024-09-08 20:01:00 -0400

    Fully completed question 1.

--- coding.3/quest1.c
+++ coding.3/quest1.c
@@ -5,14 +5,22 @@
 int main (int adgc, char *argv[]) {
     int x = 100;
     int rc = fork();
-    if (rc < 0) {
+    if (rc < 0) { // error
         fprintf(stderr, "Fork failed\n");
         exit(1);
     }
-    else if (rc == 0) {
+    else if (rc == 0) { // child
+        x += 200;
         printf("Child process value: %d\n", x);
-    } else {
+    } else { // parent
         printf("Parent process value: %d\n", x);
     }
     return 0;
 }
+
+/*********************************************
+   Your name: Logan Anglin (anglinl)
+   Question 1: Because x is initialized to 100 before the fork is created, both the parent and child processes start with x = 100. I added x += 200
+   on line 13 to verify that changing x in the child process would not change x in the parent process. Any changes in the else if and else selections
+   will only affect the variable in the child or parent processes respectively.
+*********************************************/
